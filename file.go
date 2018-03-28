5061e22952c7316c1c957a244495fc47c43bd429
Phil Porada <philporada@gmail.com>
2018-03-28 16:54:54 -0400

    test

---
+++ file.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s URL\n", os.Args[0])
+		os.Exit(1)
+	}
+	response, err := http.Get(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		defer response.Body.Close()
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//		text := string(body)
+		//		fmt.Println("response body", text)
+		n, err := html.Parse(strings.NewReader(string(body)))
+		//	if len(err) == 0 {
+		fmt.Println("ok")
+		fmt.Println(n)
+		//	} else {
+		fmt.Println(err)
+		//		}
+	}
+}
